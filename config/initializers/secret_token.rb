501d63f331a8fc4315e4d79fd189d1fe94addd81
deusse <mbevakl@gmail.com>
2013-04-15 19:04:36 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mafoto::Application.config.secret_token = '1bf4d92d7905109fc50a55ff23fd084888f248e75baaa6c7fd2e178d930928593b1ac9e1ee395ca4f8b960df4b56a2ddd7428ba4b8aedee7c3113a6d0edd7895'
